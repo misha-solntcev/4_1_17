0d263e659a1a239be8952672b4d34c201000d4be
misha-solntcev <misha.solntcev@gmail.com>
2022-10-15 16:38:51 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Вариант 17
+Из заданной матрицы A(N, M) удалите строку, в которой находится первый
+отрицательный элемент. */
+
+namespace _4_1_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
